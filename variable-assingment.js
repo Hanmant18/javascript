35863d027883b9e5c95692283e7bea8b814f3101
Hanmant18 <hkhandekar18@gmail.com>
2024-07-31 07:52:50 +0530

    javascript variable
     javascript variable

---
+++ variable-assingment.js
@@ -0,0 +1,5 @@
+console.log("My city name is Sangola");
+console.log("My best friend name is Harshvardhan");
+console.log("My country name is India");
+console.log("My school name is z.p.p school Hanmantgaon");
+console.log("My college name is Dr.Ganpatrao Deshmukh Mahavidyalay Sangola");
