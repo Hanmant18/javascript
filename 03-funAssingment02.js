42e0939dcbaf75000e3a522e5cc59240647d29f3
Hanmant18 <hkhandekar18@gmail.com>
2024-08-02 10:30:12 +0530

    03-Function Assingment 02
    03-Function Assingment 02

---
+++ 03-funAssingment02.js
@@ -0,0 +1,12 @@
+// console.log("=========2.2=========");
+// function bankDetails(bankName,accountNo,location,pinCode){
+//     console.log("function with arguments ex.bank");
+// }
+// bankDetails();
+console.log("=========Assingment 2=========");
+function bankDetails(bankName,accountNo,location,pinCode){
+    console.log("bank name is:",bankName,"||" ,"account no is:",accountNo,"||" , "location no is:",location, "||" , "pin code is:",pinCode );
+}
+bankDetails("CITI Bank", 3456782345, "Pune", 431202);
+bankDetails("AXIX Bank", 78567823456," Mumbai", 441202);
+bankDetails("HDFC Bank", 8956782345, "Pune", 631202, "Open");
