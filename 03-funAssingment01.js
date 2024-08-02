d76954c581a13aa0bb0c2fa7d1aa2d523932ba09
Hanmant18 <hkhandekar18@gmail.com>
2024-08-02 10:03:42 +0530

    03-function assingment 01
    03-function assingment 01

---
+++ 03-funAssingment01.js
@@ -0,0 +1,42 @@
+console.log("============1============");
+function king(){
+    console.log("Virat Kohli");
+}
+king();
+function hitman(){
+    console.log("Rohit Sharma");
+}
+hitman();
+
+console.log("============2============");
+
+function persnolDetails(firstName,lastName,collegeName){
+    console.log("first name is:",firstName);
+    console.log("last name is:",lastName);
+    console.log("college name is:",collegeName);
+}
+persnolDetails("hanmant","khandekar","GDMS");
+
+
+console.log("============3============");
+
+function addThreeValues(no1,no2,no3){
+    console.log("first number is:",no1);
+    console.log("second number is:",no2);
+    console.log("third number is:",no3);
+    var result=no1+no2+no3;
+    console.log("the addition is:",result);
+}
+addThreeValues(10.23,600,40);
+
+console.log("============3.3============");
+
+function addThreeValues(para1,para2,para3){
+    console.log("first parameter is:",para1);
+    console.log("second parameter is:",para2);
+    console.log("third parameter is:",para3);
+    var result=para1+para2+para3sni;
+    console.log("the result is:",result);
+}
+addThreeValues("Hello","Good","Morning");
+
